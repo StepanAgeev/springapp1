2ad98b80866c89574fe547ba8b83bde252556a8a
steff <steffan_ageev@mail.ru>
2022-11-24 20:50:16 -0500

    Test 2

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -8,7 +8,7 @@ public class TestSpring {
         TestBean testBean = context.getBean("testBean", TestBean.class);
 
         System.out.println(testBean.getName());
-
+//Test 2
         context.close();
     }
 }
